80e49d10f7d8124d1596d177bb7a75ec3e046110
hafizmuhammadsahebrazarazvi-a11y <hafizmuhammadsahebrazarazvi@gmail.com>
2025-09-03 11:36:46 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,42 @@
-import org.gradle.api.JavaVersion
-
 plugins {
-    id("com.android.application") version "8.5.1" apply false
-    kotlin("android") version "1.9.24" apply false
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.salmanilab.app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.salmanilab.app"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        debug {
+            applicationIdSuffix ".debug"
+        }
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.webkit:webkit:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
